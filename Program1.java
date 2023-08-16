3c13ef557586ca6d303fdf5937d270e22fbabee7
Nagbhushan Tirth <113358781+nt56@users.noreply.github.com>
2023-08-16 14:55:15 +0530

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,18 @@
+public class Program1 {
+
+    public static void main(String[] args){
+
+        byte w = 20;
+
+        if (w >= 0 && w <= 100){
+            if (w >= 4 && w%2 == 0){
+                System.out.println("Yes...divide by 2 and greater than four");
+            }
+            else {
+                System.out.println("No...odd Number");
+            }
+        }
+
+    }
+
+}
