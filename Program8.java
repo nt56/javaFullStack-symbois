801ebc96911278f56a8c977599f39e7c93cb42fb
Nagbhushan Tirth <113358781+nt56@users.noreply.github.com>
2023-08-22 16:57:58 +0530

    Update Program8.java

--- Program8.java
+++ Program8.java
@@ -16,3 +16,36 @@ public class Program8 {
     }
 
 }
+
+
+//Writing program for swapping the numbers using the third variable
+import java.util.Scanner;
+public class Program8 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number-1 : ");
+        int a = sc.nextInt();
+        System.out.print("Enter Number-2 : ");
+        int b = sc.nextInt();
+        System.out.println("Before Swapping a : " +a+ "b : " +b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swapping a : " +a+ "b : " +b);
+
+    }
+
+}
+
+/*
+    logic:
+    a=10,b=20
+    temp=a --> temp=10
+    a=b --> a=20
+    b=temp --> b=10
+*/
